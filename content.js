af2bdbb5e652b012e0805bd8a186c753e54d833c
Craiyo <154219004+Craiyo@users.noreply.github.com>
2024-01-12 20:16:36 +0530

    chill

--- content.js
+++ content.js
@@ -1,12 +1,7 @@
-// document.addEventListener("DOMContentLoaded", function () {
-console.log("dom content loaded");
 chrome.runtime.sendMessage({ message: "analyzeTab" });
-// });
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "extractElements") {
     console.log("inside content loaded");
-    // Your element extraction logic goes here
-    // Example: Extracting all paragraph text from the current page
     var paragraphs = document.querySelectorAll("a");
     var extractedText = Array.from(paragraphs)
       .map((p) => p.textContent)
