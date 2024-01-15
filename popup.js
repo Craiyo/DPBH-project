107833a85f45690b41b26cb4be0d1e5bd99516f9
Riyansh Sharma <Srishu215@gmail.com>
2024-01-15 16:53:55 +0530

    Commit was successful

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
+function toggleCommentBox() {
+  var commentBox = document.getElementById("commentBox");
 
+  // Toggle the display of the comment box
+  commentBox.style.display = (commentBox.style.display === "none" || commentBox.style.display === "") ? "block" : "none";
+}
