5de12011b77c098fefbf8d67044dd71940109ddc
Craiyo <154219004+Craiyo@users.noreply.github.com>
2024-01-21 22:30:23 +0530

    web examamination trial

--- content.js
+++ content.js
@@ -1,17 +1,26 @@
-chrome.runtime.sendMessage({ message: "analyzeTab" });
-// Extract elements
+// Function to extract elements
+function extractElements() {
+  console.log("Extracting elements");
+  let elements = segments(document.body);
+  const links = Array.from(document.body.querySelectorAll("a"))
+    .map((link) => link.href.trim())
+    .filter((href) => href.length > 0);
+  let filtered_elements = Array.from(elements)
+    .map((element) =>
+      element.innerText.trim().replace(/\t/g, " ").replace(/\n/g, " ")
+    )
+    .filter((text) => text.length > 0);
+
+  console.log("All elements in body tag:", elements);
+  console.log("Filtered text nodes from body tag:", filtered_elements);
+  console.log("All links in body tag:", links);
+}
+
+// Message listener
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "extractElements") {
-    console.log("Extract elements");
-    let elements = segments(document.body);
-    let filtered_elements = [];
-    for (let i = 0; i < elements.length; i++) {
-      let text = elements[i].innerText.trim().replace(/\t/g, " ");
-      if (text.length == 0) {
-        continue;
-      }
-      filtered_elements.push(text);
-    }
-    console.log(filtered_elements);
+    extractElements();
+  } else if (request.message === "otherMessage") {
   }
 });
+chrome.runtime.sendMessage({ message: "analyzeTab" });
